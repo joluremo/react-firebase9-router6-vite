bfb9cba2fcc4b7e14fec23e53a92dc6e61658938
Jose Luis Retis <joseluisretis@MacBook-Air-de-Jose.local>
2022-04-13 00:22:57 -0500

    primerazo

---
+++ src/routes/Login.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { UserContext } from "../context/UserProvider"
+
+
+const Login = () => {
+
+    const { user, setUser } = useContext(UserContext)
+    const navigate = useNavigate()
+    const handleClickLogin = () => {
+      setUser(true)
+      navigate('/')
+    }
+
+    return (
+      <>
+          <h1>Login</h1>
+          <h2>
+              {
+                  user ? 'En línea' : 'Offline'
+              }
+          </h2>
+          <button onClick={handleClickLogin}>Acceder</button>
+      </>
+    )
+  }
+  
+export default Login
